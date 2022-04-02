d060f102636940382935122b586e6e194ce9a5a7
sidra1991 <1991sidra@gmail.com>
2022-04-02 14:03:09 +0200

    creati i componenti principali

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <header-main class="center" />
+    <First-Section class="center" />
+    <available-book class="center" />
+    <testimonial-world class="center" />
+    <cards-events class="center" />
+    <subscribe-newsletter class="center" />
+    <cards-oter class="center" />
+    <recent-articles class="center" />
+    <footer-main class="center" />
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
-
-@Component({
+<script >
+import headerMain from './components/headerMain.vue'
+import FirstSection from './components/FirstSection'
+import AvailableBook from './components/availableBook.vue'
+import TestimonialWorld from './components/testimonialWorld.vue'
+import CardsEvents from './components/cardsEvents.vue'
+import SubscribeNewsletter from './components/subscribeNewsletter.vue'
+import CardsOter from './components/cardsOter.vue'
+import RecentArticles from './components/recentArticles.vue'
+import FooterMain from './components/footerMain.vue'
+export default {
   components: {
-    HelloWorld
+    headerMain,
+    FirstSection,
+    AvailableBook,
+    TestimonialWorld,
+    CardsEvents,
+    SubscribeNewsletter,
+    CardsOter,
+    RecentArticles,
+    FooterMain
   }
-})
-export default class App extends Vue {}
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .center{
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 </style>
